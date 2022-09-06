8b57a09de69e405dfb787895e2e87e04cc850c69
zengkai <979384911@qq.com>
2022-09-06 23:27:31 +0800

    添加controller

---
+++ src/main/java/org/example/yqgtest/controller/TestController.java
@@ -0,0 +1,16 @@
+package org.example.yqgtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  @GetMapping("/curl")
+  public String curl() {
+    return "success";
+  }
+
+}
