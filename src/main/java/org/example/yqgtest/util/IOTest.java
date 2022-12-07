fb0f90d0b3c8b8605e072cc21e0920dde044f5c1
zengkai <979384911@qq.com>
2022-12-07 08:55:59 +0800

    io test

---
+++ src/main/java/org/example/yqgtest/util/IOTest.java
@@ -0,0 +1,22 @@
+package org.example.yqgtest.util;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class IOTest {
+
+  public static void main(String[] args) throws Exception {
+    String test = "hhsfjkdjfkdfk\nnhdkjfjffjdfkk\n";
+    byte[] data = test.getBytes(Charset.forName("UTF-8"));
+    ByteArrayInputStream bis = new ByteArrayInputStream(data);
+    InputStreamReader inputStreamReader = new InputStreamReader(bis);
+    BufferedReader reader = new BufferedReader(inputStreamReader);
+    List<String> list = IOUtils.readLines(reader);
+  }
+
+}
