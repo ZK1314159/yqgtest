07084eb7abe08f0cb984835ae0b0b1e63b88935f
zengkai <979384911@qq.com>
2023-01-13 17:10:19 +0800

    Time test

---
+++ src/main/java/org/example/yqgtest/util/TimeTest.java
@@ -0,0 +1,16 @@
+package org.example.yqgtest.util;
+
+import com.yqg.common.util.clock.YqgClock;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+
+public class TimeTest {
+  public static void main(String[] args) {
+    LocalTime localTime = LocalTime.parse("10:00:00", DateTimeFormatter.ofPattern("HH:mm:ss"));
+    int value = localTime.get(ChronoField.MILLI_OF_DAY);
+    System.out.println(localTime.get(ChronoField.MILLI_OF_DAY));
+    System.out.println(YqgClock.getMinMillisOfDay(YqgClock.now()));
+  }
+}
