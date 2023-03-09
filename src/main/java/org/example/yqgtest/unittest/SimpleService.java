9d67c72d9a9d3a6a00643ab594ad82f02ac04aa9
zengkai <979384911@qq.com>
2023-03-09 13:22:08 +0800

    构造器注入bean测试2

--- src/main/java/org/example/yqgtest/unittest/SimpleService.java
+++ src/main/java/org/example/yqgtest/unittest/SimpleService.java
@@ -1,28 +1,30 @@
 package org.example.yqgtest.unittest;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Constructor;
+
 @Component
-//@AllArgsConstructor
-//@NoArgsConstructor
+@AllArgsConstructor
 public class SimpleService {
 
   private final MockService mockService;
   private final BeanService beanService;
 
-  public SimpleService(MockService mockService, BeanService beanService) {
-    this.mockService = mockService;
-    this.beanService = beanService;
-  }
+//  private Integer integer = 3;
+  private static String test = "fjhdf";
 
-  private Integer integer = 3;
-  private String string = "hahha";
-  private static final String test = "fjhdf";
-//  public SimpleService(MockService mockService) {
+//  public SimpleService(MockService mockService, BeanService beanService) {
 //    this.mockService = mockService;
+//    this.beanService = beanService;
 //  }
 
+
   public Integer unitTest() {
+    Class<SimpleService> simpleServiceClass = SimpleService.class;
+    Constructor[] constructors = simpleServiceClass.getDeclaredConstructors();
+    beanService.unitTest();
     return mockService.test();
   }
 
