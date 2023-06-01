392e7f8c0cc8ea691ee8aff3eb577793b5b1e232
zengkai <979384911@qq.com>
2023-06-01 23:17:50 +0800

    StringTest

---
+++ src/main/java/org/example/yqgtest/util/StringUtilsTest.java
@@ -0,0 +1,9 @@
+package org.example.yqgtest.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsTest {
+  public static void main(String[] args) {
+    String result = StringUtils.join("a", null, "c");
+  }
+}
