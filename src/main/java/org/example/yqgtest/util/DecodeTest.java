490815c3badb2e6f7c88e294897ed10fc9336769
zengkai <979384911@qq.com>
2023-07-09 11:52:44 +0800

    DecodeTest

--- src/main/java/org/example/yqgtest/util/DecodeTest.java
+++ src/main/java/org/example/yqgtest/util/DecodeTest.java
@@ -4,7 +4,7 @@ import com.yqg.common.util.hashid.YqgHashids;
 
 public class DecodeTest {
   public static void main(String[] args) {
-    System.out.println(YqgHashids.encode(225203581L));
-//    System.out.println(YqgHashids.decode("O2e0EOcJ"));
+//    System.out.println(YqgHashids.encode(225203581L));
+    System.out.println(YqgHashids.decode("xE4oT9"));
   }
 }
